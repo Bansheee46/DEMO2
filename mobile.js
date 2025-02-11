d1caba2be8296d7aa2821f5a1a39b6f023a6405b
Bansheee <timurzakharov46@yandex.ru>
2025-02-12 02:49:54 +0300

    Mobiwork2

--- mobile.js
+++ mobile.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
     const burger = document.querySelector(".burger-menu");
     const menu = document.querySelector(".b");
+    const carouselInner = document.querySelector(".carousel-inner");
+    let isDragging = false;
+    let startX, startScrollLeft;
 
     if (burger && menu) {
         burger.addEventListener("click", () => {
@@ -19,5 +22,39 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     } else {
         console.error("Элементы бургер-меню не найдены");
+    }
+
+    if (carouselInner) {
+        const dragStart = (e) => {
+            isDragging = true;
+            carouselInner.classList.add("dragging");
+            startX = e.pageX || e.touches[0].pageX;
+            startScrollLeft = carouselInner.scrollLeft;
+        };
+
+        const dragging = (e) => {
+            if (!isDragging) return;
+            e.preventDefault();
+            const x = e.pageX || e.touches[0].pageX;
+            const distance = (x - startX);
+            carouselInner.scrollLeft = startScrollLeft - distance;
+        };
+
+        const dragStop = () => {
+            isDragging = false;
+            carouselInner.classList.remove("dragging");
+        };
+
+        carouselInner.addEventListener("mousedown", dragStart);
+        carouselInner.addEventListener("touchstart", dragStart);
+
+        carouselInner.addEventListener("mousemove", dragging);
+        carouselInner.addEventListener("touchmove", dragging);
+
+        carouselInner.addEventListener("mouseup", dragStop);
+        carouselInner.addEventListener("mouseleave", dragStop);
+        carouselInner.addEventListener("touchend", dragStop);
+    } else {
+        console.error("Элементы карусели не найдены");
     }
 });
